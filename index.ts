05ddbee47394a3471a417f56eb2f5a59fc1c3124
PierrickHOUEE <houee.pierrick@gmail.com>
2022-11-18 19:02:39 +0100

    Bug solved

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
-import pupeteer from "puppeteer";
+// Removed "type": "module" from JSON to allow require
+import puppeteer from "puppeteer";
 import cheerio from "cheerio";
+
+// const puppeteer = require("puppeteer");
+// const cheerio = require("cheerio");
 
 interface oneResult {
   title: string;
@@ -13,17 +17,36 @@ let scrapingresults: oneResult[] = [];
 
 async function main() {
   // headless: false => the browser will be visible (could be unactivated when ok)
-  const browser = await pupeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  // Could add { waitUntil: "networkidle2", timeout: 0 }
-  await page.goto("https://sfbay.craigslist.org/search/sof#search=1~thumb~0~5");
+  // Works only with networkidle0 !!!
+  await page.goto(
+    "https://sfbay.craigslist.org/search/sof#search=1~thumb~0~5",
+    { waitUntil: "networkidle0", timeout: 0 }
+  );
 
   // Getting the HTML from the page
   const html = await page.content();
   const $ = cheerio.load(html);
-  $(".titlestring").each((i, e) => {
-    console.log($(e).text());
+  // const feedback = $(e).text();
+  // $(".titlestring").each((i, e) => console.log($(e).text()));
+
+  // The issue was that it wasn't getting the array associated with ".titlestring"
+  // console.log($(".titlestring").length);
+
+  // Getting the element attribute href
+  // $(".titlestring").each((i, e) => {
+  //   console.log($(e).attr("href"));
+  // });
+
+  const results = $(".titlestring").map((i, e) => {
+    const title = $(e).text();
+    const url = $(e).attr("href");
+    return { title, url };
   });
+  console.log(results);
 }
 
+// console.log("Before");
 main();
+// console.log("After");
